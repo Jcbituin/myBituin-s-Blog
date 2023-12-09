8d14c434f230a76f8ff586acfdc7102d2f01e478
Jcbituin <150402651+Jcbituin@users.noreply.github.com>
2023-12-08 17:26:34 -0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Bituin's Blog",
-    page_icon="🎥",
-)
+    page_icon="🎥",)
+
 
 # ---- LOAD ASSETS ----
 image1 = Image.open("image/GG.png")
